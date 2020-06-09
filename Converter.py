b6ed7233c58bcde6f4871e8d98e8ecaa0025d904
Javi-Ai <64416067+Javi-Ai@users.noreply.github.com>
2020-06-09 12:58:00 -0500

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -2,7 +2,7 @@ print("Convertidor de pesos Colombianos a dólares")
 
 n1=float(input("Ingrese los pesos a convertir: "))
 
-división=n1/3775
+división=n1/3611
 
 print("La conversión es igual a ",división)
 
